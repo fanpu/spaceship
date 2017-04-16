ae41bb5a93b098b268d4d0a2b44ba4f0a5c29045
Fan Pu <zengfanpu@gmail.com>
2017-04-16 23:55:29 +0800

    able to kill enemies

--- public/javascripts/spaceship.js
+++ public/javascripts/spaceship.js
@@ -64,16 +64,34 @@ function paintSpaceShip(x, y) {
   drawTriangle(x, y, 20, '#ff0000', 'up');
 }
 
+function isVisible(obj) {
+  return obj.x > -40 && obj.x < canvas.width + 40 &&
+	obj.y > -40 && obj.y < canvas.height + 40;
+}
+
 var ENEMY_FREQ = 1500;
+var ENEMY_SHOOTING_FREQ = 750;
 var Enemies = Rx.Observable.interval(ENEMY_FREQ)
 	.scan(function(enemyArray) {
 	  var enemy = {
 		x: parseInt(Math.random() * canvas.width),
 		y: -30,
+		shots: []
 	  };
 
+	  Rx.Observable.interval(ENEMY_SHOOTING_FREQ).subscribe(function() {
+		if (!enemy.isDead) {
+		  enemy.shots.push({ x: enemy.x, y: enemy.y });
+		}
+		enemy.shots = enemy.shots.filter(isVisible);
+	  });
+
 	  enemyArray.push(enemy);
-	  return enemyArray;
+	  return enemyArray
+		.filter(isVisible)
+		.filter(function(enemy) {
+		  return !(enemy.isDead && enemy.shots.length === 0);
+		});
 	}, []);
 
 function getRandomInt(min, max) {
@@ -85,15 +103,27 @@ function paintEnemies(enemies) {
 	enemy.y += 5;
 	enemy.x += getRandomInt(-15, 15);
 
-	drawTriangle(enemy.x, enemy.y, 20, '#00ff00', 'down');
+	if (!enemy.isDead) {
+	  drawTriangle(enemy.x, enemy.y, 20, '#00ff00', 'down');
+	}
+	
+	enemy.shots.forEach(function(shot) {
+	  shot.y += SHOOTING_SPEED;
+	  drawTriangle(shot.x, shot.y, 5, '#00ffff', 'down');
+	});
   });
+}
+
+function collision(target1, target2) {
+  return (target1.x > target2.x - 20 && target1.x < target2.x + 20) &&
+	(target1.y > target2.y - 20 && target1.y < target2.y + 20);
 }
 
 function renderScene(actors) {
   paintStars(actors.stars);
   paintSpaceShip(actors.spaceship.x, actors.spaceship.y);
   paintEnemies(actors.enemies);
-  paintHeroShots(actors.heroShots);
+  paintHeroShots(actors.heroShots, actors.enemies);
 }
 
 
@@ -124,14 +154,21 @@ var HeroShots = Rx.Observable
 	}, []);
 
 var SHOOTING_SPEED = 15;
-function paintHeroShots(heroShots) {
-  heroShots.forEach(function(shot) {
+function paintHeroShots(heroShots, enemies) {
+  heroShots.forEach(function(shot, i) {
+	for(var l=0; l < enemies.length; l++) {
+	  var enemy = enemies[l];
+	  if (!enemy.isDead && collision(shot, enemy)) {
+		enemy.isDead = true;
+		shot.x = shot.y = -100;
+		break;
+	  }
+	}
+
 	shot.y -= SHOOTING_SPEED;
 	drawTriangle(shot.x, shot.y, 5, '#ffff00', 'up');
   });
 }
-
-
 
 var Game = Rx.Observable
 	.combineLatest(
