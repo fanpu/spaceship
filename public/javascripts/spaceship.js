352393287518c3888d43a1f72b97e7bca29ecad7
Fan Pu <zengfanpu@gmail.com>
2017-04-16 11:00:11 +0800

    hero positioning

--- public/javascripts/spaceship.js
+++ public/javascripts/spaceship.js
@@ -38,3 +38,17 @@ function paintStars(stars) {
 	ctx.fillRect(star.x, star.y, star.size, star.size);
   });
 }
+
+var HERO_Y = canvas.height - 30;
+var mouseMove = Rx.observable.fromEvent(canvas, 'mousemove');
+var SpaceShip = mouseMove
+	.map(function(event) {
+	  return {
+		x: event.clientX,
+		y: HERO_Y
+	  };
+	})
+	.startWith({
+	  x: canvas.width / 2,
+	  y: HERO_Y
+	});
